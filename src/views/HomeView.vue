c779eaed0ce1fffbc7655720d9881ccaf321bb47
Chambers <bobhus394@gmail.com>
2022-04-19 18:05:30 +0800

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <article class=" w-11/12 mx-auto max-w-xl prose p-4">
+    <h1>Underline Animated</h1>
+    <h2>Just hover <span class="text-amber-500">amber text</span></h2>
+    <section>
+      <h2>第二個標題</h2>
+      <p>Lorem ipsum dolor sit amet <span
+          class=" text-amber-500 transition-all ease-in bg-[length:0%_3px] hover:bg-[length:100%_3px] duration-700 bg-gradient-to-r from-sky-500 to-blue-500 bg-left-bottom bg-no-repeat">consectetur
+          adipisicing elit. Natus repudiandae possimus consequuntur fugit, veniam optio temporibus magnam cupiditate
+          tenetur neque</span> laborum voluptas odio iste, numquam vitae ipsam sapiente accusantium rerum.</p>
+    </section>
+    <section>
+      <h2>第二個標題</h2>
+      <p>Lorem <span
+          class="text-amber-500 transition-all ease-in bg-[length:0%_3px] hover:bg-[length:100%_3px] duration-700 bg-gradient-to-r from-sky-500 to-blue-500 bg-left-bottom bg-no-repeat">ipsum
+        dolor sit amet consectetur adipisicing elit. Natus repudiandae possimus consequuntur fugit, veniam optio
+        temporibus magnam cupiditate tenetur neque</span> laborum voluptas odio iste, numquam vitae ipsam sapiente
+        accusantium rerum.</p>
+    </section>
+    <section>
+      <h2>第二個標題</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus repudiandae possimus consequuntur fugit, veniam
+        optio <span class="text-amber-500 transition-all ease-in bg-[length:0%_3px] hover:bg-[length:100%_3px] duration-700 bg-gradient-to-r from-sky-500 to-blue-500 bg-left-bottom bg-no-repeat">temporibus magnam</span> cupiditate tenetur neque laborum voluptas odio iste, numquam vitae ipsam sapiente
+        accusantium rerum.</p>
+    </section>
+  </article>
+</template>
+
+<style>
+</style>
